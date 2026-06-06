9692431e880932aeb4710210a0e963da62f4981b
BreadCoinyBFB <mariajuana1137@gmail.com>
2026-06-06 15:11:07 +0800

    Update main.js

--- twisters/main.js
+++ twisters/main.js
@@ -1,1 +1,15 @@
 
+const canvas = document.getElementById("game");
+
+function resizeGame() {
+    const vw = window.innerWidth;
+    const vh = window.innerHeight;
+
+    const scale = Math.min(vw / 1600, vh / 1200);
+
+    canvas.style.width = (1600 * scale) + "px";
+    canvas.style.height = (1200 * scale) + "px";
+}
+
+window.addEventListener("resize", resizeGame);
+resizeGame();
