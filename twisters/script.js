249251c0855140b9faaabafdb9d16a58afdb7f59
BreadCoinyBFB <mariajuana1137@gmail.com>
2026-06-06 17:26:32 +0800

    Remove canvas resizing functionality

--- twisters/script.js
+++ twisters/script.js
@@ -1,16 +1,1 @@
-canvas = document.getElementById("game");
- const ctx = canvas.getContext("2d");
-
-function resizeGame() {
-    const vw = window.innerWidth;
-    const vh = window.innerHeight;
-const
-    const scale = Math.min(vw / 1600, vh / 1200);
-
-    canvas.style.width = (1600 * scale) + "px";
-    canvas.style.height = (1200 * scale) + "px";
-}
-
-window.addEventListener("resize", resizeGame);
-resizeGame();
 
